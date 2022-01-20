e34ee565f0ada5ed056a1f3eaec115daab69e447
Nello-Moreira <g.moreira1@outlook.com>
2022-01-20 00:17:41 -0300

    feat: implemented room/reservation relation & room get room type and available vacancies methods

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import Hotel from "./Hotel";
+import Reservation from "./Reservation";
 
 @Entity("rooms")
 export default class Room extends BaseEntity {
@@ -17,5 +18,23 @@ export default class Room extends BaseEntity {
 
     @ManyToOne(() => Hotel, (hotel) =>  hotel.rooms)
     @JoinColumn({ name: "hotelId" })
-    hotel: Hotel
+    hotel: Hotel;
+
+    @OneToMany(() => Reservation, (reservation) =>  reservation.room, { eager: true })
+    reservations: Reservation[];
+
+    getRoomType() {
+      // rooms can have 1, 2 or 3 vacancies
+      const types: {[index: number]: string} = {
+        1: "Single",
+        2: "Double",
+        3: "Triple",
+      };
+
+      return types[this.vacancies];
+    }
+
+    getAvailableVacancies() {
+      return this.vacancies - this.reservations.length;
+    }
 }
