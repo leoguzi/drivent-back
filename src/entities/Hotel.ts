f42ae7e98c231f5e54f43f6773523921b77d5388
Nello-Moreira <g.moreira1@outlook.com>
2022-01-20 00:19:13 -0300

    feat: implemented hotel method to get available vacancies and type of rooms

--- src/entities/Hotel.ts
+++ src/entities/Hotel.ts
@@ -12,6 +12,31 @@ export default class Hotel extends BaseEntity {
     @Column()
     image: string;
 
-    @OneToMany(() => Room, (room) => room.hotel)
-    rooms: Room[]; 
+    @OneToMany(() => Room, (room) => room.hotel, { eager: true })
+    rooms: Room[];
+
+    static async getHotelTypesOfRoomsAndAvailableVacancies() {
+      function formatHotelInfos(hotel: Hotel) {
+        const roomTypes: {[index: string]: number} = {};
+        let availableVacancies = 0;
+            
+        hotel.rooms.forEach((room: Room) => {
+          const roomType = room.getRoomType();
+          roomTypes[roomType] = room.vacancies;
+          availableVacancies += room.getAvailableVacancies();
+        });
+    
+        return {
+          id: hotel.id,
+          name: hotel.name,
+          image: hotel.image,
+          roomTypes: Object.keys(roomTypes).sort((typeA, typeB) => roomTypes[typeA]-roomTypes[typeB]),
+          availableVacancies
+        };
+      }
+
+      const hotels = await Hotel.find();
+      
+      return hotels.map(hotel => formatHotelInfos(hotel));
+    }
 }
