ca05e0e6b4a4ce15e02e1aa8297e9aa0fd123830
Carlos Gabriel <carlosgabrielpc@live.com>
2022-01-21 16:53:41 -0300

    fix: add error throw when ticket online with hotel

--- src/services/client/ticket.ts
+++ src/services/client/ticket.ts
@@ -2,12 +2,18 @@ import TicketData from "@/interfaces/ticket";
 import Ticket from "@/entities/Ticket";
 import * as enrollmentService from "@/services/client/enrollment";
 import CannotBuyTicketBeforeEnrollError from "@/errors/CannotBuyTicketBeforeEnrollError";
+import CannotBuyTicketOnlineWithHotelError from "@/errors/CannotBuyTicketOnlineWithHotelError";
 
 export async function createNewTicket(ticketData: TicketData, userId: number) {
   const enrollment = await enrollmentService.getEnrollmentWithAddress(userId);
   if (!enrollment) {
     throw new CannotBuyTicketBeforeEnrollError;
   }
+
+  if (ticketData.type === "online" && ticketData.withHotel) {
+    throw new CannotBuyTicketOnlineWithHotelError;
+  }
+
   ticketData.enrollment = enrollment;
   await Ticket.createTicket(ticketData);
 }
