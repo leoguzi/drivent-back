0912edf6785ed1757814f3052f2371a9653e97cf
Carlos Gabriel <carlosgabrielpc@live.com>
2022-01-21 16:38:22 -0300

    feat: update ticket schema to allow payment date

--- src/schemas/ticketSchema.ts
+++ src/schemas/ticketSchema.ts
@@ -2,6 +2,6 @@ import joi from "joi";
 
 export default joi.object({
   type: joi.string().valid("online", "presential").required(),
-  withHotel: joi.boolean().required()
-    
+  withHotel: joi.boolean().required(),
+  paymentDate: joi.date().allow(null),
 });
