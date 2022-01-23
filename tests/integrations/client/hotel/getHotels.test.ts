cfa56cf2010ecb10760b91b97a37098805dad8f6
Nello-Moreira <g.moreira1@outlook.com>
2022-01-23 12:32:09 -0300

    feat: implemented get hotel route tests

--- tests/integrations/client/hotel/getHotels.test.ts
+++ tests/integrations/client/hotel/getHotels.test.ts
@@ -3,24 +3,120 @@ import { getConnection } from "typeorm";
 import httpStatus from "http-status";
 
 import server, { init } from "../../../../src/app";
+import User from "../../../../src/entities/User";
+import Session from "../../../../src/entities/Session";
+import Enrollment from "../../../../src/entities/Enrollment";
+import Ticket from "../../../../src/entities/Ticket";
+import Hotel from "../../../../src/entities/Hotel";
+import Room from "../../../../src/entities/Room";
+
+import { clearDatabase, clearTable } from "../../../repositories/deleterRepository";
+import createEvent from "../../../factories/event";
+import createUser from "../../../factories/user";
+import createSession from "../../../factories/session";
+import createEnrollment from "../../../factories/enrollment";
+import createHotel from "../../../factories/hotel";
+import createRoom from "../../../factories/room";
+import createTicket from "../../../factories/ticket";
+import Address from "../../../../src/entities/Address";
 
 const route = "/hotels";
 
 describe("Tests for /hotels route", () => {
+  let user: User;
+  let session: Session;
+  let enrollment: Enrollment;
+  let ticket: Ticket;
+  let hotel: Hotel;
+  let room: Room;
+  let expectedBody: any;
+
   beforeAll(async() => {
     await init();
+    await clearDatabase();
+
+    await createEvent();
+    
+    user = await createUser();
+    session = await createSession(user);
+
+    enrollment = await createEnrollment(user);
+    ticket = await createTicket(enrollment, true, true, "presencial");
+
+    hotel = await createHotel();
+    room = await createRoom(hotel);
+
+    expectedBody = [
+      {
+        id: hotel.id,
+        name: hotel.name,
+        image: hotel.image,
+        roomTypes: [
+          room.getRoomType()
+        ],
+        availableVacancies: room.vacancies
+      }
+    ];
+  });
+
+  afterEach(async() => {
+    clearTable(Ticket);
+    clearTable(Address);
+    clearTable(Enrollment);
   });
 
   afterAll(async() => {
+    await clearDatabase();
+
     await getConnection().close();
   });
 
   it("should return status code 200 (ok) and an array of hotels", async() => {
     const response = await supertest(server)
       .get(route)
-      .set("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY0MjY0MjA5OX0.HsEKA8Ff7x1_Dq2UICZZMI0g7g_DV7FF63en2GWss6A");
+      .set("authorization", `Bearer ${session.token}`);
 
-    console.log({ response: response.body });
     expect(response.status).toBe(httpStatus.OK);
+    expect(response.body).toEqual(expectedBody);
+  });
+
+  it("should return status code 403 (forbidden) when there is no enrollment", async() => {
+    const response = await supertest(server)
+      .get(route)
+      .set("authorization", `Bearer ${session.token}`);
+
+    expect(response.status).toBe(httpStatus.FORBIDDEN);
+  });
+
+  it("should return status code 403 (forbidden) when there is no ticket", async() => {
+    enrollment = await createEnrollment(user);
+
+    const response = await supertest(server)
+      .get(route)
+      .set("authorization", `Bearer ${session.token}`);
+
+    expect(response.status).toBe(httpStatus.FORBIDDEN);
+  });
+
+  it("should return status code 403 (forbidden) when ticket isn't paid yet", async() => {
+    enrollment = await createEnrollment(user);
+    ticket = await createTicket(enrollment, true, false, "presencial");
+
+    const response = await supertest(server)
+      .get(route)
+      .set("authorization", `Bearer ${session.token}`);
+
+    expect(response.status).toBe(httpStatus.FORBIDDEN);
+  });
+
+  it("should return status code 403 (forbidden) when ticket doesn't include hotel reservation", async() => {
+    enrollment = await createEnrollment(user);
+    ticket = await createTicket(enrollment, false, true, "presencial");
+
+    const response = await supertest(server)
+      .get(route)
+      .set("authorization", `Bearer ${session.token}`);
+
+    expect(response.status).toBe(httpStatus.FORBIDDEN);
   });
 });
